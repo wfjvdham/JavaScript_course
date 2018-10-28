8c22ce18d4e2defdd75bae7eec2ec8ff9980e0fa
Wim <wfjvdham@gmail.com>
2018-10-28 09:46:19 +0100

    move example app to answers

---
+++ answers/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+window.onload = function() {
+  const bookTitles = [
+    // Replace with your own book titles
+    'harry_potter_chamber_secrets'
+  ];
+
+
+  // Replace with your own code
+  console.log(bookTitles);
+
+  let books_div = document.getElementById("books");
+  console.log(books_div);
+  books_div.textContent = bookTitles;
+}
