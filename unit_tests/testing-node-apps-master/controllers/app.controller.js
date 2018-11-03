cf82fb03ff7c9c041a4979805e7583d21600b1c0
Wim <wfjvdham@gmail.com>
2018-11-03 10:51:53 +0100

    add unit test example

---
+++ unit_tests/testing-node-apps-master/controllers/app.controller.js
@@ -0,0 +1,8 @@
+
+// A func that takes in two parameters `req` and `res` [request, response]
+exports.getIndexPage = function(req, res){
+  if (req.user.isLoggedIn()) {
+    return res.send("Hey");
+  }
+  res.send("Ooops. You need to log in to access this page");
+}
